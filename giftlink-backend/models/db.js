9b5c86cb354847faac70abb67b44e92ae236c192
xolela booi <xbooi023@student.wethinkcode.co.za>
2026-05-22 09:46:58 +0200

    updates

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -2,27 +2,18 @@
 require('dotenv').config();
 const MongoClient = require('mongodb').MongoClient;
 
-// MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
-
 let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
-
-    const client = new MongoClient(url);      
-
-    // Task 1: Connect to MongoDB
-    // {{insert code}}
-
-    // Task 2: Connect to database giftDB and store in variable dbInstance
-    //{{insert code}}
-
-    // Task 3: Return database instance
-    // {{insert code}}
+    if (dbInstance) {
+        return dbInstance;
+    }
+    const client = new MongoClient(url);
+    await client.connect();
+    dbInstance = client.db(dbName);
+    return dbInstance;
 }
 
 module.exports = connectToDatabase;
